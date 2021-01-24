b04071ec6cea15cb2d8a7d4bfc906668dcd3f606
Ane Acevedo <ane.acevedo@gmail.com>
2021-01-24 18:36:11 -0300

    Solucionando deploy pantalla blanca

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Pokemon from './Page/Pokemon.js';
 import Header from './Components/Header/Header';
 
